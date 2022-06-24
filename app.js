04d9a05704f8b7af95db053a84fb7c76bddc4e5a
Yasin Yağmur <yasin.yagmur@gmail.com>
2022-06-24 15:19:21 +0300

    js file edited

--- app.js
+++ app.js
@@ -33,3 +33,19 @@ reset.addEventListener("click", () => {
   minutes = 0;
 });
 
+function watchStart() {
+  milliseconds += 1;
+  if (milliseconds == 100) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+    }
+  }
+
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  let ms = milliseconds < 10 ? "0" + milliseconds : milliseconds;
+  watchDiv.innerHTML = ` ${m} : ${s} : ${ms}`;
+}
